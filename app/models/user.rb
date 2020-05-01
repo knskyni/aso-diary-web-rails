2622b877e8a3cb7ca0c0d411f087213d67e9aedf
Kensuke Yanai <knskyni@yanai-k.com>
2020-05-01 15:52:26 +0900

    add: UserモデルにValidatesを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+    validates :student_id, {presence: true, uniqueness: true, length: {is: 7}}
+    validates :first_name, {presence: true, length: {maximum: 16}}
+    validates :last_name, {presence: true, length: {maximum: 16}}
+    validates :email, {presence: true, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}}
+    validates :password, {presence: true, length: {minimum: 8}}
 end

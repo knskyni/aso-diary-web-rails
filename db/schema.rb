1677235d6a8d54cae1de10f893680c8fddf524cc
yanakence <yanakence@yanah.jp>
2020-05-06 02:05:29 +0900

    feat: Diary Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_05_021616) do
+ActiveRecord::Schema.define(version: 2020_05_05_165222) do
+
+  create_table "diaries", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "class_id"
+    t.integer "student_id"
+    t.date "date"
+    t.boolean "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "good_comment"
+    t.string "bad_comment"
+    t.string "about_comment"
+    t.integer "teacher_id"
+    t.string "teacher_comment"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "student_id"

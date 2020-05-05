5fda6d7a18194511d47b597684d570e2fa4bcaca
Kensuke Yanai <knskyni@yanai-k.com>
2020-05-05 11:20:52 +0900

    add: BCryptの導入

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_30_074517) do
+ActiveRecord::Schema.define(version: 2020_05_05_021616) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "student_id"
     t.string "first_name"
     t.string "last_name"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

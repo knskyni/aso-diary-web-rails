98d1cc00c083cb0dc0e1bc9b7e4cb75145cbfe93
yanakence <yanakence@yanah.jp>
2020-05-06 01:36:22 +0900

    feat: ログイン処理を実装

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,12 +1,22 @@
 class UserController < ApplicationController
-  # route(get): /login
+  # get: /login
   def login_form
-      @user = User.new
+    @user = User.new
     render layout: false
   end
 
-  #route(post): /login
+  # post: /login
   def login
-
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to("/diary/index")
+    else
+      @user = User.new
+      @error_message = "メールアドレスまたはパスワードが間違っています。"
+      @email = params[:email]
+      @password = params[:password]
+      render("user/login_form", layout: false)
+    end
   end
 end

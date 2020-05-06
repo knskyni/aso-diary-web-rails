7b49973cd0817915905bba8b3859050d7f8405e9
Kensuke Yanai <knskyni@yanai-k.com>
2020-05-06 11:57:10 +0900

    fix: ログインページで値の受け取りが正しくできていない問題を修正

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -7,15 +7,15 @@ class UserController < ApplicationController
 
   # post: /login
   def login
-    @user = User.find_by(email: params[:email])
-    if @user && @user.authenticate(params[:password])
+    @user = User.find_by(email: params[:user][:email])
+    if @user && @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
       redirect_to("/diary/index")
     else
       @user = User.new
       @error_message = "メールアドレスまたはパスワードが間違っています。"
-      @email = params[:email]
-      @password = params[:password]
+      @email = params[:user][:email]
+      @password = params[:user][:password]
       render("user/login_form", layout: false)
     end
   end

afdea1918cce7db4c265b3811fb744e93119bbf2
Kensuke Yanai <knskyni@yanai-k.com>
2020-05-07 11:24:27 +0900

    feat: Diaryモデルにおけるバリデーションを実装した

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,4 +1,8 @@
 class Diary < ApplicationRecord
+  validates :good_comment, {presence: true, length: {maximum: 1024, minimum: 16}}
+  validates :bad_comment, {presence: true, length: {maximum: 1024, minimum: 16}}
+  validates :about_comment, {presence: true, length: {maximum: 1024, minimum: 16}}
+
   def student
     return User.find_by(student_id: self.student_id)
   end

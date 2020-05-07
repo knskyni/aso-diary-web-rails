0058fb34ba976a4add3bfd5c38f57b75c1b50d0d
yanakence <yanakence@yanah.jp>
2020-05-07 12:55:05 +0900

    feat: 日誌の登録確認画面の機能部分を実装した

--- app/controllers/diary_controller.rb
+++ app/controllers/diary_controller.rb
@@ -11,6 +11,20 @@ class DiaryController < ApplicationController
 
   # post: /diary/confirm
   def new_confirm
+    @diary = Diary.new(student_id: @current_user.student_id)
+    @diary.good_comment = params[:diary][:good_comment]
+    @diary.bad_comment = params[:diary][:bad_comment]
+    @diary.about_comment = params[:diary][:about_comment]
+
+    if @diary.valid?
+      session[:diary] = @diary
+    else
+      render("diary/new")
+    end
+  end
+
+  # post: /diary/create
+  def create
 
   end
 end
